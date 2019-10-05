6873e15a9805f0d7fac5243c9eb43a33320a2db8
julien demarque <julien_demarque@hotmail.fr>
2019-10-05 14:47:46 +0300

    feat: add data from old project

---
+++ data/javascript/minus-operator-coercion/question.js
@@ -0,0 +1,3 @@
+// What will console.log from the following code;
+
+console.log(true - false, 2 - "foo", "3" - 1);
---
+++ data/javascript/plus-operator-coercion/question.js
@@ -0,0 +1,3 @@
+// What will console.log from the following code;
+
+console.log(true + 0, true + "foo", "bar" + 5);
